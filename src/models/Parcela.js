082a373fec65a50a20e572d130584894f8351952
Henrique Baldin Rosa <henribaldin@hotmail.com>
2018-08-09 21:31:33 -0300

    Adição de getters e toJSON no objeto da parcela

--- src/models/Parcela.js
+++ src/models/Parcela.js
@@ -1,10 +1,40 @@
 class Parcela {
-    constructor(mes, prestacao, juros, amortizacao, saldoDevedor) {
-    	this._mes = mes;
-    	this._prestacao = prestacao;
-    	this._juros = juros;
-    	this._amortizacao = amortizacao;
-    	this._saldoDevedor = saldoDevedor;
-    	Object.freeze(this);
-    }
+	constructor(mes, prestacao, juros, amortizacao, saldoDevedor) {
+		this._mes = mes;
+		this._prestacao = prestacao;
+		this._juros = juros;
+		this._amortizacao = amortizacao;
+		this._saldoDevedor = saldoDevedor;
+		Object.freeze(this);
+	}
+
+	get mes() {
+		return this._mes;
+	}
+
+	get prestacao() {
+		return this._prestacao;
+	}
+
+	get juros() {
+		return this._juros;
+	}
+
+	get amortizacao() {
+		return this._amortizacao;
+	}
+
+	get saldoDevedor() {
+		return this._saldoDevedor;
+	}
+
+	toJSON() {
+		return {
+			mes: this.mes,
+			prestacao: this.prestacao,
+			juros: this.juros,
+			amortizacao: this.amortizacao,
+			saldoDevedor: this.saldoDevedor
+		};
+	}
 }
